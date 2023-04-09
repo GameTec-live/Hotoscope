48832d307c4ef5f62a3a697ba945a3f0465c7a55
robotto <mark@moore.dk>
2023-04-09 22:30:16 +0200

    Rotated OLED 180 degrees

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ Thermistor* thermistor;
 #include <Wire.h>
 #include <U8g2lib.h>
 
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R2, /* reset=*/ U8X8_PIN_NONE);
 
 void setup() {
   // Setup Thermistor
